809663e348b2c69a192105a64f3cce7351b7680a
myoung <myoungsc.dev@gmail.com>
2018-07-04 11:36:48 +0900

    - A Member function that Add border line only the desire direction

---
+++ ENG/CALayerExtension_ENG.swift
@@ -0,0 +1,39 @@
+//
+//  CALayerExtension_KOR.swift
+//  dfae
+//
+//  Created by maccli1 on 2018. 7. 4..
+//  Copyright © 2018년 myoung. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//MARK: ## CALayer Extension ##
+extension CALayer {
+    
+    func borderLineDirection(_ directions: [UIRectEdge], color: UIColor, thickness: CGFloat) {
+        /*
+         - A Member function that Add border line only the desire direction
+         - Return type is nil
+         - yourView.layer.borderLineDirection([.top, .bottom], color: UIColor.blue, thickness: 1.0)
+        */
+        for direction in directions {
+            let border = CALayer()
+            switch direction {
+            case UIRectEdge.top:
+                border.frame = CGRect(x: 0, y: 0, width: frame.width, height: thickness)
+            case UIRectEdge.bottom:
+                border.frame = CGRect(x: 0, y: frame.height-thickness, width: frame.width, height: thickness)
+            case UIRectEdge.left:
+                border.frame = CGRect(x: 0, y: 0, width: thickness, height: frame.height)
+            case UIRectEdge.right:
+                border.frame = CGRect(x: frame.width-thickness, y: 0, width: thickness, height: frame.height)
+            default: break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+    
+}
