755e97d1992437a64791cdf024acc018d4d7d5ab
myoung <myoungsc.dev@gmail.com>
2018-07-19 12:11:40 +0900

    - add function that hide line from navigationbar - add function that show line from navigationbar

---
+++ en/UINavigationBarExtension.swift
@@ -0,0 +1,44 @@
+//
+//  UINavigationBarExtension.swift
+//  dfae
+//
+//  Created by maccli1 on 2018. 7. 19..
+//  Copyright © 2018년 myoung. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationBar {
+    
+    func showBorderLine() {
+        /*
+         - A function that show lines from NavigationBar
+         - Return type is nil
+         - ex) self.navigationController?.navigationBar.showBorderLine() -> line show
+        */
+        findBorderLine().isHidden = false
+    }
+    
+    func hideBorderLine() {
+        /*
+         - A function that hide lines from NavigationBar
+         - Return type is nil
+         - ex) self.navigationController?.navigationBar.hideBorderLine() -> line hidden
+        */
+        findBorderLine().isHidden = true
+    }
+    
+    private func findBorderLine() -> UIImageView! {
+        /*
+         - A method that find UIImageView from NavigationBar
+         - Return type is 'UIImageView!'
+        */
+        
+        return self.subviews
+            .flatMap { $0.subviews }
+            .compactMap { $0 as? UIImageView }
+            .filter { $0.bounds.size.width == self.bounds.size.width }
+            .filter { $0.bounds.size.height <= 2 }
+            .first
+    }
+}
---
+++ ko/UINavigationBarExtension.swift
@@ -0,0 +1,43 @@
+//
+//  UINavigationBarExtension.swift
+//  dfae
+//
+//  Created by maccli1 on 2018. 7. 19..
+//  Copyright © 2018년 myoung. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationBar {
+    
+    func showBorderLine() {
+        /*
+         - NavigationBar에 있는 라인 보여주는 멤버 함수
+         - 반환값 타입은 없음
+         - ex) self.navigationController?.navigationBar.showBorderLine() -> line show
+        */
+        findBorderLine().isHidden = false
+    }
+    
+    func hideBorderLine() {
+        /*
+         - NavigationBar에 있는 라인 숨겨주는 멤버 함수
+         - 반환값 타입은 없음
+         - ex) self.navigationController?.navigationBar.hideBorderLine() -> line hidden
+        */
+        findBorderLine().isHidden = true
+    }
+    
+    private func findBorderLine() -> UIImageView! {
+        /*
+         - navigationbar에서 UIImageView 찾아주는 메소드
+         - 반환값 타입은 UIImageView!
+        */
+        return self.subviews
+            .flatMap { $0.subviews }
+            .compactMap { $0 as? UIImageView }
+            .filter { $0.bounds.size.width == self.bounds.size.width }
+            .filter { $0.bounds.size.height <= 2 }
+            .first
+    }
+}
